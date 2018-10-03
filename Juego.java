e258b2937952ca0b98aaf916c83051622cb00197
ajneb97 <benjamin_ld@hotmail.com>
2018-10-03 20:45:15 -0300

    Add files via upload

--- Juego.java
+++ Juego.java
@@ -5,17 +5,19 @@ import java.util.Random;
 public class Juego {
 
 	private ArrayList<String> palabras;
+	private ArrayList<String> letrasUsadas;
 	private Palabra palabra;
 	private Conector conector;
 	private boolean finalizado;
+	private int fallos;
 	
 	public Juego() throws IOException {
 		palabras = new ArrayList<String>();
+		letrasUsadas = new ArrayList<String>();
 		finalizado = false;
-		palabras.add("PERRO");
+		fallos = 0;
 		palabras.add("GALLETA");
 		palabras.add("REDES");
-		palabras.add("ARBOLEDA");
 		Random r = new Random();
 		int pos = r.nextInt(palabras.size()-1);
 		this.palabra = new Palabra(palabras.get(pos));
@@ -30,26 +32,80 @@ public class Juego {
 		System.out.println("(Si obtienes 3 fallos, pierdes el juego.)");
 		System.out.println("");
 		System.out.println("ENTER para Comenzar.");
-		conector.getEntrada().readLine();
-		
-		System.out.println("");
-		System.out.println("PALABRA: "+palabra.getLineas());
-		System.out.println("");
-		while(!finalizado) {
+		conector.getEntrada().readLine();	
+		while(!finalizado) {	
+			mostrarPalabra();
 			preguntaLetra();
+			
 		}
+		conector.cerrar();
 	}
 	
 	public void preguntaLetra() throws IOException {
-            String letra;
-            do {
-		System.out.println("Escribe una letra");
-		letra = conector.getEntrada().readLine();
-                if (!letra.matches("^[a-zA-Z]")) {
-                    System.out.println("Por favor asegurate de que estes ingresando una letra.");
-                    
-                }
-            }while (!letra.equals("")) ;
 		
+		System.out.println("");
+		String letra = getLetra();
+		//AQUI COMPROBACIONES DE QUE SEA VERDADERAMENTE UNA LETRA, NO UNA CADENA DE STRINGS, NO UN NUMERO, NO VACIO
+		//PUEDE CREARSE UNA CLASE APARTE DE UTILIDADES PARA VERIFICAR ESTO
+		if(!letrasUsadas.contains(letra)) {
+			letrasUsadas.add(letra);
+			if(palabra.letraEstaEnPalabra(letra)) {
+				palabra.setLetra(letra);
+				System.out.println("CORRECTO! La letra "+letra+" si esta en la palabra.");
+				if(palabra.isPalabraCompleta()) {
+					System.out.println("¡HAS GANADO! La palabra era: "+palabra.getTexto());
+					finalizado = true;
+				}
+			}else {
+				falloLetra(letra);
+				if(fallos == 3) {
+					//Terminar juego y conexion
+					System.out.println("¡HAS PERDIDO!");
+					finalizado = true;
+				}
+			}
+		}else {
+			System.out.println("Ya has usado la letra "+letra+", intenta con otra.");
+		}
+		
+	}
+	
+	public String getLetra() throws IOException {
+        String letra;
+        do {
+        	System.out.println("Escribe una letra");
+        	letra = conector.getEntrada().readLine();
+            if (!letra.matches("^[a-zA-Z]")) {
+                System.out.println("Por favor asegurate de que estes ingresando una letra.");
+                
+            }
+        }while (!letra.matches("^[a-zA-Z]")) ;
+        return letra.toUpperCase();
+	}
+	
+	public void mostrarPalabra() {
+		System.out.println("____________________________________");
+		System.out.println("");
+		System.out.println("PALABRA: "+palabra.getLineas());
+		if(!letrasUsadas.isEmpty()) {
+			String letras = "(";
+			for(int i=0;i<letrasUsadas.size();i++) {
+				if(i==letrasUsadas.size()-1) {
+					letras = letras+letrasUsadas.get(i);
+				}else {
+					letras = letras+letrasUsadas.get(i)+",";
+				}
+				
+			}
+			System.out.println("Letras Usadas: "+letras+")");
+		}
+		System.out.println("Fallos: ("+this.fallos+")");
+		System.out.println("");
+		System.out.println("____________________________________");
+	}
+	
+	public void falloLetra(String letra) {
+		fallos++;
+		System.out.println("FALLO! La letra "+letra+" no esta en la palabra. Ahora tienes: "+fallos+" fallo(s).");		
 	}
 }
--- Palabra.java
+++ Palabra.java
@@ -12,6 +12,10 @@ public class Palabra {
 		}
 	}
 	
+	public String getTexto() {
+		return this.texto;
+	}
+	
 	public String getLineas() {
 		String lineas = "";
 		for(int i=0;i<this.espacios.size();i++) {
@@ -22,5 +26,36 @@ public class Palabra {
 			}
 		}
 		return lineas;
+	}
+	
+	public boolean letraEstaEnPalabra(String letra) {
+		if(texto.contains(letra)) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+	
+	public void setLetra(String letra) {
+		for(int i=0;i<texto.length();i++) {
+			if(texto.substring(i, i+1).equals(letra)) {
+				espacios.set(i, letra);
+			}
+		}
+	}
+	
+	public boolean isPalabraCompleta() {
+		int cont = 0;
+		for(int i=0;i<this.espacios.size();i++) {
+			if(!this.espacios.get(i).equals("-1")) {
+				cont++;
+			}
+		}
+		
+		if(texto.length() == cont) {
+			return true;
+		}else {
+			return false;
+		}
 	}
 }
